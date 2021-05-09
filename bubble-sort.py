e2aee07086f45c46da0eeeb9909e28e6f5ec6e38
Igor <igorgras100@gmail.com>
2021-05-09 17:38:12 +0000

    Quick Sort implemented

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,10 +1,10 @@
+from swap import swap
+
 def bubble_sort(A):
     for i in range(len(A)):
         for j in range(len(A) - 1):
             if A[j] > A[j+1]:
-                temp = A[j]
-                A[j] = A[j+1]
-                A[j+1] = temp
+                swap(A, j, j+1)
 
 myArr = [5,4,2,7,9,1,8,3,6]
 print(myArr)
--- quick-sort.py
+++ quick-sort.py
@@ -1,19 +1,19 @@
 from swap import swap
 
 def quick_sort(A, p, r):
-    if not p == r:
-        q = partition(A, p, r);
-        print(A)
-        print(p , q, r)
-        quick_sort(A, p, q)
-        quick_sort(A, q+1, r)
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q  -1)
+        quick_sort(A, q, r)
 
 def partition(A, p, r):
     q = p - 1
-    for i in range(p, r+1):
+    for i in range(p, r):
         if A[i] <= A[r]:
             q += 1
             swap(A, i, q)
+    q += 1
+    swap(A, r, q)
     return q
 
 myArr = [5,4,2,7,9,1,8,3,6]
