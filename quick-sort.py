f0ab40097f9b03e50e752ab40aceca71c62502dc
Igor <igorgras100@gmail.com>
2021-05-09 19:33:02 +0000

    Fixed bug in quick sort algorithm

--- quick-sort.py
+++ quick-sort.py
@@ -12,8 +12,8 @@ def quick_sort(A, p, r):
         return q
     if p < r:
         q = partition(A, p, r)
-        quick_sort(A, p, q  -1)
-        quick_sort(A, q, r)
+        quick_sort(A, p, q-1)
+        quick_sort(A, q+1, r)
 
 myArr = [5,4,2,7,9,1,8,3,6]
 print(myArr)
