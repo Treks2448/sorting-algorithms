ae547efd12f3990e0f1823489490ac3f6a15b5e5
Igor <igorgras100@gmail.com>
2021-05-09 18:39:04 +0000

    Tidied up quick-sort.py code

--- quick-sort.py
+++ quick-sort.py
@@ -1,20 +1,19 @@
 from swap import swap
 
 def quick_sort(A, p, r):
+    def partition(A, p, r):
+        q = p - 1
+        for i in range(p, r):
+            if A[i] <= A[r]:
+                q += 1
+                swap(A, i, q)
+        q += 1
+        swap(A, r, q)
+        return q
     if p < r:
         q = partition(A, p, r)
         quick_sort(A, p, q  -1)
         quick_sort(A, q, r)
-
-def partition(A, p, r):
-    q = p - 1
-    for i in range(p, r):
-        if A[i] <= A[r]:
-            q += 1
-            swap(A, i, q)
-    q += 1
-    swap(A, r, q)
-    return q
 
 myArr = [5,4,2,7,9,1,8,3,6]
 print(myArr)
